455287be743338ea18a38683857a6179cb95f580
u1aryz <u1aryz.d@gmail.com>
2023-05-13 22:59:59 +0900

    トークンの有効期限が切れた時に更新する処理を追加

--- src/useToken.ts
+++ src/useToken.ts
@@ -1,13 +1,31 @@
 import { useIdToken } from "react-firebase-hooks/auth";
-import { useAsync } from "react-use";
-import { Auth } from "firebase/auth";
+import { Auth, User } from "firebase/auth";
+import { useEffect } from "react";
+
+type AuthUser = User & {
+	accessToken: string;
+	stsTokenManager: {
+		expirationTime: number;
+	};
+};
 
 export const useToken = (
 	auth: Auth,
 ): [string | undefined, boolean, Error | undefined] => {
-	const [user, idTokenLoading, idTokenError] = useIdToken(auth);
-	const { value, loading, error } = useAsync(async () => {
-		return user?.getIdToken();
-	}, [user]);
-	return [value, idTokenLoading || loading, idTokenError || error];
+	const [user, loading, error] = useIdToken(auth);
+	const token = (user as AuthUser)?.accessToken;
+
+	useEffect(() => {
+		// https://github.com/CSFrequency/react-firebase-hooks/issues/299
+		if (user && token) {
+			const authUser = user as AuthUser;
+			const tokenLife = authUser.stsTokenManager.expirationTime - Date.now();
+			const timer = setTimeout(() => {
+				authUser.getIdToken(true).catch(console.error);
+			}, tokenLife);
+			return () => clearTimeout(timer);
+		}
+	}, [user, token]);
+
+	return [token, loading, error];
 };
